e67a41528db1f48a913f667434256e625fbaffa6
Walid Sabihi <walid.openrealms@gmail.com>
2016-09-05 17:41:53 +0100

    Set babel/react loaders in web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,17 @@ module.exports = {
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
-    }
+    },
+    module: {
+      loaders: [
+        {
+          test: /.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react']
+          }
+        }
+      ]
+    },
 }
