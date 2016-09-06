ea9a1511a9865be6e959c160341a5ae10792f77d
Walid Sabihi <walid.openrealms@gmail.com>
2016-09-06 10:43:23 +0100

    Sandbox/Input basic logic

--- components/input.js
+++ components/input.js
@@ -3,17 +3,26 @@ import React from 'react'
 import Button from './button'
 
 class SandboxInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.submitInput = this.submitInput.bind(this)
+  }
   render() {
     return (
       <div>
         <label htmlFor="inputTextarea">Input:</label>
         <br/>
-        <textarea id="inputTextarea" rows="10" cols="100">
+        <textarea ref={(c) => this._input = c} id="inputTextarea" rows="10" cols="100">
         </textarea>
         <br/>
-        <Button onClick={null} disabled={false} />
+        <Button onClick={this.submitInput} disabled={false} />
       </div>
     )
+  }
+  submitInput() {
+    let inputCode = this._input.value
+    let modifiedCode = inputCode.replace(new RegExp("console.log", "g"), "logOutput")
+    this.props.submitCode(modifiedCode)
   }
 }
 
--- components/sandbox.js
+++ components/sandbox.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
 import SandboxInput from './input'
+import SandboxOutput from './output'
 
 class Sandbox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.runCode = this.runCode.bind(this)
+  }
   render() {
     return (
       <div>
-        <SandboxInput />
+        <SandboxInput ref={(c) => this.inputTextarea = c} submitCode={this.runCode}/>
+        <br />
+        <SandboxOutput ref={(c) => this.outputTextarea = c} />
       </div>
     )
+  }
+  runCode(code) {
+    this.outputTextarea.clearLines()
+    window.logOutput = window.logOutput.bind(this)
+    window.logException = window.logException.bind(this)
+    try {
+      eval(code)
+    } catch (e) {
+      logException(e)
+    }
   }
 }
 
