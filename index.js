4f087009c60e4ded7c27a2c5ed72cdfbce9f6546
Walid Sabihi <walid.openrealms@gmail.com>
2016-09-06 10:43:38 +0100

    Implement logOutput/logException/defineToJson

--- index.js
+++ index.js
@@ -10,6 +10,41 @@ function renderYawpSandbox(element) {
   )
 }
 
+//This function is bound to the Sandbox component
+function logOutput(output) {
+  this.outputTextarea.appendLine(JSON.stringify(output))
+}
+
+//This function is bound to the Sandbox component
+function logException(output) {
+  let stringifiedError = JSON.stringify(output)
+  let modifiedError = stringifiedError.replace(new RegExp("\\\\n", "g"), "\n")
+  let finalError = "Exception occured:\n" + modifiedError
+  this.outputTextarea.setState({value: finalError})
+}
+
+function defineToJson() {
+  if (!('toJSON' in Error.prototype))
+  Object.defineProperty(Error.prototype, 'toJSON', {
+      value: function () {
+          var alt = {}
+
+          Object.getOwnPropertyNames(this).forEach(function (key) {
+              alt[key] = this[key];
+          }, this)
+
+          return alt;
+      },
+      configurable: true,
+      writable: true
+  })
+}
+
+defineToJson()
+
+window.logOutput = logOutput
+window.logException = logException
+
 window.React = React;
 window.renderYawpSandbox = renderYawpSandbox
 
